1af6447a57041c1d36f8fb76991f979bf7910174
Roderick Nuque <enuke4@gmail.com>
2021-12-23 19:11:33 +0000

    add reverseString.js algorithm

---
+++ free-code-camp/reverseString.js
@@ -0,0 +1,25 @@
+//Reverse a String Algorithm
+
+function reverseString(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+//Other way to solve this problem by leveraging the built-in methods of JS
+/*
+function reverseString(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+*/
+
+//Tests
+console.log(reverseString('my name is erick'));
+console.log(reverseString('hello'));
+console.log(reverseString('philippines'));
+
